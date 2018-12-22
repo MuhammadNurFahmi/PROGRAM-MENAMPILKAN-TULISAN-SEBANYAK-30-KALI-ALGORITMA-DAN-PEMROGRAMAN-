33de6597714b20fe61f5ac957da83ba4d08d8ad0
MuhammadNurFahmi <45891167+MuhammadNurFahmi@users.noreply.github.com>
2018-12-22 20:32:48 +0700

    Add files via upload

---
+++ 30 kali.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	int c; //c = char(CHARACTER)
+	for (c=0;c<30;c++) //JUMLAH HURUF YANG INGIN DITAMPILKAN
+	{
+		cout<<"ALGORITMA DAN PEMROGRAMAN " <<endl; //KATA YANG INGIN DITMPILKAN
+		
+	}
+return 0;
+}
